5ddf4b24d1dac94282779ff51a1a01ffe1107c05
Muskan-1808 <muskangarg1808@gmail.com>
2026-06-08 01:11:27 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.title("🩺 Diabetes Predictor Web App")
+st.markdown("### 🔐 Doctor Login")
+st.caption("AI-based prediction using patient health data")
+
+USERNAME = "doctor"
+PASSWORD = "1234"
+
+u = st.text_input("Username")
+p = st.text_input("Password", type="password")
+
+if st.button("Login"):
+    if u == USERNAME and p == PASSWORD:
+        st.session_state["logged_in"] = True
+        st.switch_page("pages/predict_diabetes.py")
+    else:
+        st.error("Invalid credentials")
