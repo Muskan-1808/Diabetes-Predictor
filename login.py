21d863a812463353b18f4e9d67a0d028a6fc5450
Muskan-1808 <muskangarg1808@gmail.com>
2026-06-08 13:13:02 +0530

    Add files via upload

--- login.py
+++ login.py
@@ -13,6 +13,6 @@ p = st.text_input("Password", type="password")
 if st.button("Login"):
     if u == USERNAME and p == PASSWORD:
         st.session_state["logged_in"] = True
-        st.switch_page("predict_diabetes.py")
+        st.switch_page("pages/predict_diabetes.py")
     else:
         st.error("Invalid credentials")
